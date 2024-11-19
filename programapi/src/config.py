71d8c076169bd58496a1e10cd79b8aa3641d407c
Cristián Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2024-11-20 00:19:26 +0100

    WIP: Add programapi
    This is required to improve the JSON API from pretalx, creating
    a more complete set of JSON files with the information from
    sessions, speakers, and schedule.
    
    The sessions JSON file is an entry point for the Discord Bot
    in order to do the announcement of talks.

---
+++ programapi/src/config.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+class Config:
+    event = "pyladiescon-2024"
+    project_root = Path(__file__).resolve().parents[1]
+    raw_path = Path(f"{project_root}/data/raw/{event}")
+    public_path = Path(f"{project_root}/data/public/{event}")
+
+    @classmethod
+    def token(cls) -> str:
+        dotenv_exists = load_dotenv(cls.project_root / ".env")
+        if (token := os.getenv("PRETALX_TOKEN")) and not dotenv_exists:
+            print("Please prefer .env file to store your token! It's more secure!")
+            return token
+        elif token is None:
+            raise ValueError("Please set your token in .env file!")
+        return token
---
+++ programapi/src/utils/transform.py
@@ -0,0 +1,153 @@
+from src.models.europython import (
+    EuroPythonScheduleBreak,
+    EuroPythonScheduleSession,
+    EuroPythonScheduleSpeaker,
+    EuroPythonSession,
+    EuroPythonSpeaker,
+    Schedule,
+)
+from src.models.pretalx import PretalxScheduleBreak, PretalxSpeaker, PretalxSubmission
+from src.utils.timing_relationships import TimingRelationships
+from src.utils.utils import Utils
+
+
+class Transform:
+    @staticmethod
+    def pretalx_submissions_to_europython_sessions(
+        submissions: dict[str, PretalxSubmission],
+        youtube_data: dict[str, str] = None,
+    ) -> dict[str, EuroPythonSession]:
+        """
+        Transforms the given Pretalx submissions to EuroPython sessions
+        """
+        # Sort the submissions based on start time for deterministic slug computation
+        submissions = {
+            k: v
+            for k, v in sorted(
+                submissions.items(),
+                key=lambda item: (item[1].start is None, item[1].start),
+            )
+        }
+
+        session_code_to_slug = Utils.compute_unique_slugs_by_attribute(
+            submissions, "title"
+        )
+
+        ep_sessions = {}
+        for code, submission in submissions.items():
+            ep_session = EuroPythonSession(
+                code=submission.code,
+                title=submission.title,
+                speakers=submission.speakers,
+                session_type=submission.submission_type,
+                slug=session_code_to_slug[submission.code],
+                track=submission.track,
+                abstract=submission.abstract,
+                duration=submission.duration,
+                resources=submission.resources,
+                room=submission.room,
+                start=submission.start,
+                end=submission.end,
+                answers=submission.answers,
+                sessions_in_parallel=TimingRelationships.get_sessions_in_parallel(
+                    submission.code
+                ),
+                sessions_after=TimingRelationships.get_sessions_after(submission.code),
+                sessions_before=TimingRelationships.get_sessions_before(
+                    submission.code
+                ),
+                next_session=TimingRelationships.get_next_session(submission.code),
+                prev_session=TimingRelationships.get_prev_session(submission.code),
+                slot_count=submission.slot_count,
+                #youtube_url=youtube_data.get(submission.code),
+                youtube_url="None",
+            )
+            ep_sessions[code] = ep_session
+
+        return ep_sessions
+
+    @staticmethod
+    def pretalx_speakers_to_europython_speakers(
+        speakers: dict[str, PretalxSpeaker],
+    ) -> dict[str, EuroPythonSpeaker]:
+        """
+        Transforms the given Pretalx speakers to EuroPython speakers
+        """
+        # Sort the speakers based on code for deterministic slug computation
+        speakers = {k: v for k, v in sorted(speakers.items(), key=lambda item: item[0])}
+
+        speaker_code_to_slug = Utils.compute_unique_slugs_by_attribute(speakers, "name")
+
+        ep_speakers = {}
+        for code, speaker in speakers.items():
+            ep_speaker = EuroPythonSpeaker(
+                code=speaker.code,
+                name=speaker.name,
+                biography=speaker.biography,
+                avatar=speaker.avatar,
+                slug=speaker_code_to_slug[speaker.code],
+                answers=speaker.answers,
+                submissions=speaker.submissions,
+            )
+            ep_speakers[code] = ep_speaker
+
+        return ep_speakers
+
+    @staticmethod
+    def pretalx_schedule_to_europython_schedule(
+        breaks: list[PretalxScheduleBreak],
+        ep_sessions: dict[str, EuroPythonSession],
+        ep_speakers: dict[str, EuroPythonSpeaker],
+    ) -> Schedule:
+        """
+        Transforms the given Pretalx schedule to EuroPython schedule
+        """
+
+        # Merge breaks with the same start and end times
+        breaks = Utils.merge_breaks(breaks)
+        ep_breaks = []
+        for title, duration, rooms, start in breaks:
+            ep_break = EuroPythonScheduleBreak(
+                title=title,
+                duration=duration,
+                rooms=rooms,
+                start=start,
+            )
+            ep_breaks.append(ep_break)
+
+        # Split the sessions that covers multiple slots
+        ep_schedule_sessions_split = []
+        for session in ep_sessions.values():
+            # Skip the sessions that are not assigned in the schedule
+            if not session.start or not session.room:
+                continue
+            start_times = Utils.start_times(session)
+            for start_time in start_times:
+                ep_schedule_session = EuroPythonScheduleSession(
+                    code=session.code,
+                    slug=session.slug,
+                    title=session.title,
+                    session_type=session.session_type,
+                    speakers=[
+                        EuroPythonScheduleSpeaker(
+                            code=speaker_code,
+                            name=ep_speakers[speaker_code].name,
+                            avatar=ep_speakers[speaker_code].avatar,
+                            slug=ep_speakers[speaker_code].slug,
+                            website_url=ep_speakers[speaker_code].website_url,
+                        )
+                        for speaker_code in session.speakers
+                    ],
+                    track=session.track,
+                    #tweet=session.tweet,
+                    level=session.level,
+                    total_duration=int(session.duration),
+                    rooms=[session.room],
+                    start=start_time,
+                    website_url=session.website_url,
+                    slot_count=session.slot_count,
+                )
+
+                ep_schedule_sessions_split.append(ep_schedule_session)
+
+        return Schedule.from_events(ep_breaks + ep_schedule_sessions_split)

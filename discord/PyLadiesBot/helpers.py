d8f25c7048850c46a5c948347414267f046237a6
Adam Piskorski <adam@piskorski.me>
2025-05-19 20:50:27 -0400

    feat: Add discord logger and one use of it in the ping cog

---
+++ discord/PyLadiesBot/helpers.py
@@ -0,0 +1,78 @@
+import logging
+import textwrap
+
+from configuration import Config
+from discord import Message
+from discord.abc import Messageable
+from discord.ext.commands import Bot
+
+config = Config()
+
+
+async def safe_send_message(target: Messageable, text: str) -> list[Message]:
+    """Safely send a message to the given messageable target.
+    The utility of this is function, is that the message will be split into multiple parts
+    if its too long.
+    """
+    messages = []
+    for part in textwrap.wrap(
+        text,
+        width=2000,
+        expand_tabs=False,
+        replace_whitespace=False,
+        drop_whitespace=False,
+    ):
+        message = await target.send(part)
+        messages.append(message)
+    return messages
+
+
+class DiscordLogger:
+    """Wrapper for the configured project logger, that also sends the same logs to discord.
+
+    Use any of the logging level methods, just like in the standard library on the
+    instantiated object.
+
+    Automatically prefixes `bot.` to the name provided for the logger.
+
+    Requires a bot object to be instantiated and passed into it.
+
+    For example:
+    ```python
+        discord_logger = DiscordLogger(__name__, self.bot)
+        discord_logger.info("The 'ping' command has been triggered!")
+        discord_logger.error("A problem has occurred!")
+    ```
+    """
+
+    def __init__(self, name: str, bot: Bot):
+        self.name = f"bot.{name}"
+        self.bot = bot
+        self.logger = logging.getLogger(self.name)
+
+    def __getattr__(self, method_name: str):
+        logging_types = {
+            "debug",
+            "info",
+            "warning",
+            "warn",
+            "error",
+            "exception",
+            "critical",
+            "fatal",
+        }
+        if method_name not in logging_types:
+            raise AttributeError(f"Logging type must be one of {logging_types}")
+
+        def _log(message: str):
+
+            async def inner():
+                logging_method = getattr(self.logger, method_name)
+                logging_method(message)
+                channel = self.bot.get_channel(config.log_channel)
+                prefix = f"{self.name} - {method_name.upper()} - "
+                await safe_send_message(channel, f"{prefix}{message}")
+
+            return inner()
+
+        return _log
